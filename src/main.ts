64acf5ab3fe3c7b8012c6d45d474caaace5d4ab0
Vinh Ta <tavinh132@gmail.com>
2024-10-11 21:29:20 -0700

    Step 5 complete - purchasing an upgrade

--- src/main.ts
+++ src/main.ts
@@ -8,11 +8,13 @@ let n1: number = 0;
 let n2: number = 0;
 let currentGrowth: number = 0;
 
+let upgradeValue01: number = 1;
+
 //const interval01Array = []; //don't need
 //display variables
 const gameName = "The eyeball game";
 const buttonEmoji = "👁️";
-let eyeballDisplay = `there are ${counter} eyeballs`;
+let eyeballDisplay = `there are ${Math.floor(counter)} eyeballs`;
 
 //interval01Array.push(autoClicker(1, 1000)); //don't need
 
@@ -20,28 +22,37 @@ document.title = gameName;
 
 //createElement's parameter dictates what kind of element it is
 const gameTitle = document.createElement("h1");
-const button01 = document.createElement("button");
+const clickButton = document.createElement("button");
 const eyeballCounter = document.createElement("div");
+const upgradeButton01 = document.createElement("button");
 
 //changing the innerHTML changes what the element should display
 gameTitle.innerHTML = gameName;
-button01.innerHTML = buttonEmoji;
+clickButton.innerHTML = buttonEmoji;
 eyeballCounter.innerHTML = eyeballDisplay;
+upgradeButton01.innerHTML = "open your eyes.";
 
 //adds it to the page, underneath the previous appended thing
 app.append(gameTitle);
 app.append(eyeballCounter);
-app.append(button01);
+app.append(clickButton);
+app.append(upgradeButton01);
 
 //starting growth
 autoClicking();
 
 //listeners
-button01.addEventListener("mouseup", () => {
-    currentGrowth ++;
-  //addToCounter(1);
-  testLog("button01. currentGrowth");
+clickButton.addEventListener("mouseup", () => {
+  addToCounter(1);
+
+  //testLog("button01");
 });
+
+upgradeButton01.addEventListener("mouseup", () => {
+    currentGrowth += upgradeValue01;
+    counter -= 10;
+
+})
 
 
 
@@ -57,8 +68,11 @@ function addToCounter(toAdd: number) {
 }
 
 function updateDisplay() {
-  eyeballDisplay = `there are ${counter} eyeballs`;
+  eyeballDisplay = `there are ${Math.floor(counter)} eyeballs`;
   eyeballCounter.innerHTML = eyeballDisplay;
+
+//enable/disable upgrades
+    if (counter < 10) {upgradeButton01.disabled  = true} else {upgradeButton01.disabled = false}
 }
 
 //remaking
@@ -68,13 +82,13 @@ function updateDisplay() {
 // }
 
 function autoClicking() {
-    n2 = performance.now();
-    const msPassed = n2 - n1;
-    console.log(currentGrowth);
-    addToCounter((currentGrowth * msPassed) / 1000); // 1/fps
-    n1 = n2;
-//   requestAnimationFrame(() => autoClicking(incrementValue)); //fucks up without having this call back change
-//   //() => to make a function, then pass incrementValue back to itself
-//   //or else it increases on its own????? fuck you
-    requestAnimationFrame(autoClicking) //nevermind
+  n2 = performance.now();
+  const msPassed = n2 - n1;
+  //console.log(currentGrowth);
+  addToCounter((currentGrowth * msPassed) / 1000); // 1/fps
+  n1 = n2;
+  //   requestAnimationFrame(() => autoClicking(incrementValue)); //fucks up without having this call back change
+  //   //() => to make a function, then pass incrementValue back to itself
+  //   //or else it increases on its own????? fuck you
+  requestAnimationFrame(autoClicking); //nevermind
 }
